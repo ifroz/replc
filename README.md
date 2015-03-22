39cbc7df9121a668419554e5e28f8f3d29190b74
ifroz <ifrozen@gmail.com>
2015-03-22 03:18:28 +0100

    replc 2

--- README.md
+++ README.md
@@ -1,26 +1,47 @@
 # `replc`
 
-`replc` aims to be something like the `rails console`.
+`replc` aims to be something like the `rails console`, for iojs. It operates in your current working directory.
 
-## Usage
+## Usage in code
 
-Simplest: 
-```
-var repl = require('node-repl');
-repl();
-```
-
-Configured: 
-```
-var config = {
-  useDevDependencies: true,
+```javascript
+var replc = require('replc');
+replc({
   context: {
+    anythingYouWouldUse: 42,
     customFunction: () => console.log('whatever');
-  }
-};
-repl(config);
+  },
+  useDevDependencies: true
+});
 ```
 
 ### Config parameters
 - `context`: any objects in here will be accessible in your REPL.
-- `useDependencies`: all `package.json` dependencies will be available 
+- `aliases`: require dependencies to a different name, e.g.: `{lodash: '_'}`
+- `dependencies`: an optional array of package names, if you don't want to `useDependencies`.
+- `useDependencies`: all `package.json` dependencies will be available, `true` by default
+- `useDevDependencies`: all `package.json` devDependencies will be available, `false` by default,
+- `replOptions`: additional params given to the underlying `repl`.
+- `useColors`: `true` by default
+- `silent`: suppress welcome message, false by default
+
+An example config would look like this:
+```javascript
+var defaultConfig = {
+  context: { log: console.log },
+  useDependencies: true,
+  useDevDependencies: pkg.name === 'replc',
+  useColors: true,
+  silent: false,
+  dependencies: ['lodash', 'moment', 'string', 'co'],
+  aliases: {
+    lodash: '_',
+    underscore: '_',
+    string: 'S'
+  },
+  replOptions: {
+    prompt: pkg.name + '#> '
+  },
+  debugMode: pkg.name === 'replc'
+};
+```
--- replc.js
+++ replc.js
@@ -3,11 +3,13 @@ var _ = require('lodash'),
     repl = require('repl'),
     colors = require('colors');
 var pkg = require(process.cwd() + '/package.json');
+console.log(pkg);
 
 var defaultConfig = {
   context: { log: console.log },
+  path: process.cwd(),
   useDependencies: true,
-  useDevDependencies: pkg.name === 'repl',
+  useDevDependencies: pkg.name === 'replc',
   useColors: true,
   silent: false,
   dependencies: ['lodash', 'moment', 'string', 'co'],
@@ -19,8 +21,23 @@ var defaultConfig = {
   replOptions: {
     prompt: pkg.name + '#> '
   },
-  debugMode: pkg.name === 'repl'
+  debugMode: pkg.name === 'replc'
 };
+
+function replc(inputConfig) {
+  var cfg = _.defaults(inputConfig || {}, pkg.repl, defaultConfig);
+  var ctx = renderContext(cfg);
+
+  if (!_.isEmpty(ctx) && ! cfg.silent) {
+    getPrinter(cfg)(getWelcomeMessage(ctx));
+  }
+
+  var replServer = repl.start(cfg.replOptions);
+  _.assign(replServer.context, ctx);
+}
+
+module.exports = replc;
+if (_.contains(process.argv, '--replc')) replc();
 
 function renderContext(cfg) {
   var ctx = tryToRequireAll(cfg.dependencies, cfg.aliases);
@@ -33,12 +50,13 @@ function renderContext(cfg) {
   if (cfg.debugMode) {
     ctx._cfg = cfg;
   }
-  _.assign(ctx, cfg.context)
+  _.assign(ctx, cfg.context);
   return ctx;
 }
 
 function tryToRequireAll(packages, aliases, useKeys) {
   return _.reduce(packages, function requirePackages(modules, value, key) {
+
     var moduleName = useKeys ? key : value;
     try {
       modules[aliases[moduleName] || moduleName] = require(moduleName);
@@ -47,7 +65,11 @@ function tryToRequireAll(packages, aliases, useKeys) {
   }, {});
 }
 
-function getWelcomeString(ctx) {
+function getPrinter(cfg) {
+  return cfg.useColors ? _.flow(colors.blue, console.log) : console.log;
+}
+
+function getWelcomeMessage(ctx) {
   return [
     'Package: ' + pkg.name,
     'Path: ' + process.cwd(),
@@ -55,17 +77,4 @@ function getWelcomeString(ctx) {
     'Context: ' + _.keys(ctx).join(', ')
   ].join('\n');
 }
-
-module.exports = function replc(inputConfig) {
-  var cfg = _.defaults(inputConfig || {}, pkg.repl, defaultConfig);
-  var ctx = renderContext(cfg);
-
-  if (!_.isEmpty(ctx) && ! cfg.silent) {
-    var print = cfg.useColors ? _.flow(colors.blue, console.log) : console.log;
-    print(getWelcomeString(ctx));
-  }
-
-  var replServer = repl.start(cfg.replOptions);
-  return _.assign(replServer, {context: ctx});
-};
 
