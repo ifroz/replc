3a69a90f8244c3c146a4030eb6f346cb41141470
ifroz <ifrozen@gmail.com>
2015-03-22 11:03:46 +0100

    cleanup

--- README.md
+++ README.md
@@ -11,37 +11,36 @@ replc({
     anythingYouWouldUse: 42,
     customFunction: () => console.log('whatever');
   },
+  packages: ['fs', 'lodash'],
   useDevDependencies: true
 });
 ```
 
 ### Config parameters
 - `context`: any objects in here will be accessible in your REPL.
-- `aliases`: require dependencies to a different name, e.g.: `{lodash: '_'}`
+- `aliases`: require dependencies to a different name, e.g.: `{lodash: '__'}` Note that `_` contains the result of the last expression.
 - `dependencies`: an optional array of package names, if you don't want to `useDependencies`.
 - `useDependencies`: all `package.json` dependencies will be available, `true` by default
 - `useDevDependencies`: all `package.json` devDependencies will be available, `false` by default,
 - `replOptions`: additional params given to the underlying `repl`.
+- `logger`: defaults to `console.log`
 - `useColors`: `true` by default
 - `silent`: suppress welcome message, false by default
 
 An example config would look like this:
 ```javascript
 var defaultConfig = {
-  context: { log: console.log },
+  context: {log: console.log},
+  logger: console.log,
+  path: process.cwd(),
   useDependencies: true,
   useDevDependencies: pkg.name === 'replc',
   useColors: true,
   silent: false,
-  dependencies: ['lodash', 'moment', 'string', 'co'],
-  aliases: {
-    lodash: '_',
-    underscore: '_',
-    string: 'S'
-  },
-  replOptions: {
-    prompt: pkg.name + '#> '
-  },
+  dependencies: ['fs', 'lodash', 'moment', 'string', 'co'],
+  aliases: { lodash: '__', underscore: '__', string: 'S' },
+  // _ has special value in the node repl.
+  replOptions: { prompt: pkg.name + '#> ' },
   debugMode: pkg.name === 'replc'
 };
 ```
--- replc.js
+++ replc.js
@@ -3,41 +3,34 @@ var _ = require('lodash'),
     repl = require('repl'),
     colors = require('colors');
 var pkg = require(process.cwd() + '/package.json');
-console.log(pkg);
 
 var defaultConfig = {
-  context: { log: console.log },
+  context: {log: console.log},
+  logger: console.log,
   path: process.cwd(),
   useDependencies: true,
   useDevDependencies: pkg.name === 'replc',
   useColors: true,
   silent: false,
-  dependencies: ['lodash', 'moment', 'string', 'co'],
-  aliases: {
-    lodash: '_',
-    underscore: '_',
-    string: 'S'
-  },
-  replOptions: {
-    prompt: pkg.name + '#> '
-  },
+  dependencies: ['fs', 'lodash', 'moment', 'string', 'co'],
+  aliases: { lodash: '__', underscore: '__', string: 'S' }, // _ has special value in repl
+  replOptions: { prompt: pkg.name + '#> ' },
   debugMode: pkg.name === 'replc'
 };
 
 function replc(inputConfig) {
-  var cfg = _.defaults(inputConfig || {}, pkg.repl, defaultConfig);
-  var ctx = renderContext(cfg);
+  var config = configWithDefaults(inputConfig);
+  var context = renderContext(config);
 
-  if (!_.isEmpty(ctx) && ! cfg.silent) {
-    getPrinter(cfg)(getWelcomeMessage(ctx));
-  }
+  getPrinter(config)(getWelcomeMessage(context));
 
-  var replServer = repl.start(cfg.replOptions);
-  _.assign(replServer.context, ctx);
+  var replServer = repl.start(config.replOptions);
+  return _.assign(replServer.context, context);
 }
 
-module.exports = replc;
-if (_.contains(process.argv, '--replc')) replc();
+function configWithDefaults(yourConfig) {
+  return _.assign({}, yourConfig, pkg.repl, defaultConfig);
+}
 
 function renderContext(cfg) {
   var ctx = tryToRequireAll(cfg.dependencies, cfg.aliases);
@@ -50,14 +43,12 @@ function renderContext(cfg) {
   if (cfg.debugMode) {
     ctx._cfg = cfg;
   }
-  _.assign(ctx, cfg.context);
-  return ctx;
+  return _.assign(ctx, cfg.context);
 }
 
 function tryToRequireAll(packages, aliases, useKeys) {
   useKeys = useKeys !== undefined ? useKeys : _.isObject(useKeys);
-  return _.reduce(packages, function requirePackages(modules, value, key) {
-
+  return _.reduce(packages, function requirePackage(modules, value, key) {
     var moduleName = useKeys ? key : value;
     try {
       modules[aliases[moduleName] || moduleName] = require(moduleName);
@@ -66,16 +57,18 @@ function tryToRequireAll(packages, aliases, useKeys) {
   }, {});
 }
 
-function getPrinter(cfg) {
-  return cfg.useColors ? _.flow(colors.blue, console.log) : console.log;
+function getPrinter(cfg, color) {
+  if (cfg.silent) return _.noop;
+  return cfg.useColors ? _.flow(colors[color || 'blue'], cfg.logger) : cfg.logger;
 }
-
-function getWelcomeMessage(ctx) {
+function getWelcomeMessage(context) {
   return [
     'Package: ' + pkg.name,
     'Path: ' + process.cwd(),
     'Args: ' + process.argv.slice(2),
-    'Context: ' + _.keys(ctx).join(', ')
+    'Context: ' + _.keys(context).join(', ')
   ].join('\n');
 }
 
+module.exports = replc;
+if (_.contains(process.argv, '--replc')) { replc(); }
